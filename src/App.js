957336ed0c8abb85ffd086cf15c2a1d95847c13a
Raz Kap <johndoe@example.com>
2022-01-05 13:43:31 +0200

    added travis file2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Kapuya 444s!</p>
+        <p>Kapuya 4s!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
